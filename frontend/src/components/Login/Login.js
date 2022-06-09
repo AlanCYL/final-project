e018aa623ea38d25d752d7ecc043452e0ee009a8
remi <verayyone@gmail.com>
2022-06-10 01:49:56 +0800

    [UPDATE]login.js

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,9 +1,43 @@
 import React from 'react'
+function Login() {
+  return (
+    <>
+      <form>
+        {/* Email帳號 */}
+        <div className="mb-4">
+          <label className="form-label" for="">
+            帳號
+          </label>
+          <input type="email" className="form-control form-control-lg" />
+        </div>
 
-const Login = () => {
-  return <>
-    
-  </>
+        {/* 密碼 */}
+        <div className="mb-4">
+          <label className="form-label" for="">
+            密碼
+          </label>
+          <input type="password" className="form-control form-control-lg" />
+        </div>
+
+        <div className="d-flex justify-content-between mb-3">
+          <a href="/">忘記密碼</a>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" />
+            <label className="form-check-label" for="">
+              記住我
+            </label>
+          </div>
+        </div>
+
+        {/* login button */}
+        <div className="d-grid gap-2 mx-auto">
+          <button className="btn btn-danger" type="button">
+            登入
+          </button>
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default Login
