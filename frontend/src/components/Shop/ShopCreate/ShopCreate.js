c498ddf6472e8dc8f8dbdeb45ae3cdc3059d42c0
remi <verayyone@gmail.com>
2022-06-10 12:43:07 +0800

    [UPLOAD]shopCreate page

--- frontend/src/components/Shop/ShopCreate/ShopCreate.js
+++ frontend/src/components/Shop/ShopCreate/ShopCreate.js
@@ -1,4 +1,6 @@
 function ShopCreate() {
-  return <></>
+  return <>
+    
+  </>
 }
 export default ShopCreate
--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import NavbarShop from '../components/Navbar/NavbarShop'
-import ShopLogin from '../components/Shop/ShopLogin/ShopLogin'
+// import ShopLogin from '../components/Shop/ShopLogin/ShopLogin'
 import ShopCreate from '../components/Shop/ShopCreate/ShopCreate'
 function Shop() {
   return (
     <>
       <NavbarShop />
-      <ShopLogin />
+      {/* <ShopLogin /> */}
       <ShopCreate />
     </>
   )
