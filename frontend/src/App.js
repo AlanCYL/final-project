fd7800f840fdca4c02ff67f437156f5202eb37ad
jayna922 <leeyijie922@gmail.com>
2022-06-09 22:43:13 -0700

    [UPLOAD]
    create header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Nav from './components/Navbar/NavbarDesktop'
+import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import MemberLogin from './pages/MemberLogin'
 function App() {
   return (
     <>
       <Nav />
+      <Header />
       <MemberLogin />
       <Footer />
     </>
