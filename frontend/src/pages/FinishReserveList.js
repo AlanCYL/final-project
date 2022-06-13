cf5716437100755a08dfd65c69559cf3a81d1312
jayna922 <leeyijie922@gmail.com>
2022-06-13 16:45:05 -0700

    [UPLOAD]create card component

--- frontend/src/pages/FinishReserveList.js
+++ frontend/src/pages/FinishReserveList.js
@@ -11,7 +11,7 @@ const FinishReserveList = () => {
             <div className="w-75 ms-8" style={{ marginRight: '80px' }}>
               <img
                 style={{ width: '500px' }}
-                className="mb-5"
+                className="mb-5 me-5"
                 src={require('../image/shoppingCart/icongroup06.png')}
                 alt=""
               />
@@ -30,13 +30,13 @@ const FinishReserveList = () => {
               </h3>
 
               <div className="w-75">
-                <h6 className="mb-4">訂單編號：</h6>
-                <div className="d-flex mb-4">
+                <h6 className="">訂單編號：</h6>
+                <div className="d-flex mt-4 pb-4 border-bottom">
                   <div className="ms-6">000001</div>
                   <div className="ms-5">鼎泰豐 (2022/06/17)</div>
                   <div className="ms-5">人數 : 1</div>
                 </div>
-                <div className="d-flex mb-4">
+                <div className="d-flex mt-4 pb-4 border-bottom">
                   <div className="ms-6">000002</div>
                   <div className="ms-5">夏慕尼 (2022/06/18)</div>
                   <div className="ms-5">人數 : 1</div>
@@ -44,6 +44,18 @@ const FinishReserveList = () => {
               </div>
             </div>
           </div>
+        </div>
+        {/*  */}
+        <div className="d-flex justify-content-around mt-4">
+          <a type="button" className="bg-info text-white px-4 py-2 ms-6 mt-4">
+            取消訂位
+          </a>
+          <a
+            type="button"
+            className="bg-primary text-white px-4 py-2 me-5 mt-4"
+          >
+            前往結帳
+          </a>
         </div>
       </div>
     </>
