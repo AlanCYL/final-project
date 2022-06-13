4ad61ed771a46ab5a3e9f8fc78b7d8318e66611b
jayna922 <leeyijie922@gmail.com>
2022-06-13 09:57:58 -0700

    [UPDATE]APP.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import Nav from './components/Navbar/NavbarDesktop'
 import Footer from './components/Footer/Footer'
 // import MemberLogin from './pages/MemberLogin'
-// import Shop from './pages/Shop'
-import EmptyReserveList from './pages/EmptyReserveList'
+import Shop from './pages/Shop'
+// import ReserveList from './components/ShoppingCart/ReserveList'
 function App() {
   return (
     <>
       <Nav />
       {/* <MemberLogin /> */}
-      {/* <Shop /> */}
-      <EmptyReserveList />
+      <Shop />
+      {/* <ReserveList /> */}
       <Footer />
     </>
   )
