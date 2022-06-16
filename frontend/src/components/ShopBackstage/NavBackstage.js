b947780af4f5d664d6ff0cae9dd4d597d7fcd254
jayna922 <leeyijie922@gmail.com>
2022-06-16 10:59:41 -0700

    [FIX]shopBackstage's Nav

---
+++ frontend/src/components/ShopBackstage/NavBackstage.js
@@ -0,0 +1,61 @@
+import Col from 'react-bootstrap/Col'
+import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row'
+import Tab from 'react-bootstrap/Tab'
+import Opengroup from './Opengroup'
+
+const BackstageNav = () => {
+  return (
+    <>
+      <div className="w-100">
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col sm={3} className="bg-secondary py-3 pe-0">
+              <Nav variant="pills" className="flex-column">
+                <h3 className="text-center my-4">鼎泰豐後台管理</h3>
+                <Nav.Item>
+                  <Nav.Link eventKey="first" className="text-center py-4">
+                    基本資料
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second" className="text-center py-4">
+                    我要開團
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third" className="text-center py-4">
+                    歷史開團
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="forth" className="text-center py-4">
+                    上架菜色
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="fifth" className="text-center py-4">
+                    店家評價
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">{/* < 基本資料 /> */}</Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <Opengroup />
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">{/* <Sonnet /> */}</Tab.Pane>
+                <Tab.Pane eventKey="forth">{/* <Sonnet /> */}</Tab.Pane>
+                <Tab.Pane eventKey="fifth">{/* <Sonnet /> */}</Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </div>
+    </>
+  )
+}
+
+export default BackstageNav
---
+++ frontend/src/components/ShopBackstage/try.js
@@ -0,0 +1,16 @@
+;<div className="w-25 bg-secondary">
+  <div className="pt-3">
+    <h4 className="p-2 w-100 text-center">鼎泰豐後台管理</h4>
+
+    {/* <BiStore /> */}
+    <h5 className="px-2 py-4 sb-mb sb-hover w-100 text-center">基本資料</h5>
+
+    <h5 className="px-2 py-4 sb-mb sb-hover w-100 text-center">我要開團</h5>
+
+    <h5 className="px-2 py-4 sb-mb sb-hover w-100 text-center">歷史開團</h5>
+
+    <h5 className="px-2 py-4 sb-mb sb-hover w-100 text-center">上架菜色</h5>
+
+    <h5 className="px-2 py-4 sb-mb sb-hover w-100 text-center">店家評價</h5>
+  </div>
+</div>
