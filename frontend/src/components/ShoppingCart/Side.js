58feeeceded3c4048b50eff9aba7ad9f8a8c3ef3
jayna922 <leeyijie922@gmail.com>
2022-06-16 23:19:19 -0700

    [FIX]side's scss

--- frontend/src/components/ShoppingCart/Side.js
+++ frontend/src/components/ShoppingCart/Side.js
@@ -4,13 +4,13 @@ const Side = () => {
   return (
     <>
       <div className="side w-25 ms-3">
-        <div className="d-flex mt-3">
+        <div className="d-flex mt-3 ">
           <img src={require('../../image/shoppingCart/cart.png')} alt="" />
-          <h5 className="mx-1">購物車</h5>
+          <h5 className="mx-1 side-hover">購物車</h5>
         </div>
-        <div className="d-flex mt-5">
+        <div className="d-flex mt-5 ">
           <img src={require('../../image/shoppingCart/heart.png')} alt="" />
-          <h5 className="mx-1">收藏清單</h5>
+          <h5 className="mx-1 side-hover">收藏清單</h5>
         </div>
       </div>
     </>
