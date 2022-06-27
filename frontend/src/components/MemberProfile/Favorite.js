81d208c6e68061ca63c097bd1bc5251f780a08d9
Alan <0527alan.lin@gmail.com>
2022-06-27 20:16:32 +0800

    [UPLOAD]member page

--- frontend/src/components/MemberProfile/Favorite.js
+++ frontend/src/components/MemberProfile/Favorite.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -6,38 +6,82 @@ import { Link } from 'react-router-dom'
 import Badge from 'react-bootstrap/Badge'
 import { FaHeart } from 'react-icons/fa'
 import { useEffect } from 'react'
+import { useLogin } from '../../context/LoginStatus'
+import axios from 'axios'
+import { API_URL, IMAGE_URL } from '../../utils/config'
+import { useFavorite } from '../../context/FavoriteList'
 
 const Favorite = () => {
-  // TODO::
-  // useEffect(()=>{
+  const { member } = useLogin()
+  const [myFavorite, setMyFavorite] = useState([])
+  const { setCountFavorite } = useFavorite()
+  const [cancel, setCancel] = useState(true)
 
-  // })
+  useEffect(() => {
+    ;(async () => {
+      try {
+        let response = await axios.get(`${API_URL}/favorite/list`, {
+          params: {
+            userId: member.id,
+          },
+        })
+        setMyFavorite(response.data.favorite)
+        setCountFavorite(response.data.count)
+      } catch (e) {
+        console.log(e.response.data.error)
+      }
+    })()
+  }, [member.id, cancel])
+
+  const deleteFavorite = async (id) => {
+    try {
+      let response = await axios.get(`${API_URL}/favorite/delete`, {
+        params: {
+          shopId: id,
+          userId: member.id,
+        },
+      })
+      setCancel(!cancel)
+    } catch (e) {
+      console.log(e.response.data.error)
+    }
+  }
 
   return (
     <>
       <Container className="mb-3">
         <Row xs={1} md={2} lg={3} className="g-5">
           {/* 店家卡片 */}
-          {[1, 1, 1].map((v, i) => {
+          {myFavorite.map((v, i) => {
             return (
-              <Col className="d-flex justify-content-center">
+              <Col className="d-flex justify-content-center" key={v.id}>
                 <div className="text-center">
                   <div className="position-relative">
                     <div className="rounded-circle bg-white text-danger h5 position-absolute top-0 start-100 translate-middle heart">
-                      <FaHeart className="heart_pic" />
+                      <FaHeart
+                        className="heart_pic"
+                        onMouseUp={() => {
+                          deleteFavorite(v.shop_id)
+                        }}
+                      />
                     </div>
 
                     <img
-                      className="w-100"
-                      src={require('../../image/shopList/Kangyaolife-1.jpg')}
-                      alt=""
+                      className="w-100 shadow-sm"
+                      src={IMAGE_URL + `/shopImg/${v.shopImg}`}
+                      alt={v.shopImg}
                     />
                   </div>
                   <div className="shopLink">
                     <Link to="/shopListDetail" className="d-block">
-                      <h5 className="my-2">陶板屋</h5>
-                      <Badge bg="primary d-inline">中式</Badge>{' '}
-                      <Badge bg="primary d-inline">自助餐</Badge>{' '}
+                      <h5 className="my-2">{v.shopName}</h5>
+                      {v.typeName.split(',').map((v, i) => {
+                        return (
+                          <Badge className="mx-1" bg="primary d-inline" key={i}>
+                            {v}
+                          </Badge>
+                        )
+                      })}
                     </Link>
                   </div>
                 </div>
