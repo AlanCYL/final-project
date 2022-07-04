ce44751def7bde554972eea1a9243c635dc684de
jayna922 <leeyijie922@gmail.com>
2022-07-04 15:01:34 -0700

    [UPLOAD]shoppingcart

--- frontend/src/components/ShoppingCart/Pay/FinishPay.js
+++ frontend/src/components/ShoppingCart/Pay/FinishPay.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../../../components/Card/Card'
+import { useHistory } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { API_URL, IMAGE_URL } from '../../../utils/config'
 
@@ -7,6 +7,7 @@ const FinishPay = () => {
   const payGroup = localStorage.getItem('payGroup')
   const userID = localStorage.getItem('userID')
   const [data, setData] = useState({})
+  const history = useHistory()
 
   useEffect(() => {
     fetch(
@@ -35,7 +36,7 @@ const FinishPay = () => {
   }
   return (
     <>
-      <div className=" container my-6 ">
+      <div className=" container my-4 ">
         <div className="d-flex justify-content-center mb-8">
           <div>
             <div className="w-75 ms-8" style={{ marginRight: '80px' }}>
@@ -111,15 +112,117 @@ const FinishPay = () => {
                 className="bg-primary"
               ></div>
             </div>
-            <div className="d-flex mt-4 ">
-              <div className="me-4 mb-8">
-                <Card />
+            <div className="d-flex mt-4 justify-content-center">
+              <div className="me-4 ">
+                {/* card */}
+                <div
+                  className="little-card shadow-sm bg-white rounded main-hover"
+                  onClick={() => {
+                    history.push('/groupDetail/12')
+                  }}
+                >
+                  <img
+                    className="w-100 h-50 rounded-top"
+                    src={require('../../../image/shopList/Kangyaolife-1.jpg')}
+                    alt=""
+                  />
+                  <div className="p-3">
+                    <div className="d-flex justify-content-between">
+                      <h4>陶板屋</h4>
+                      <span class="badge rounded-pill bg-primary">日式</span>
+                    </div>
+                    <h6 className="fw-normal">目前人數:0人</h6>
+                    <h6 className="fw-normal mb-4">用餐時間:2022/07/19</h6>
+                    <div class="progress">
+                      <div
+                        class="progress-bar progress-bar-striped"
+                        role="progressbar"
+                        style={{ width: '30%' }}
+                        aria-valuenow="0"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ></div>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                      <p className="progress-text mt-1">剩下6天</p>
+                      <p className="progress-text mt-1">目標人數:4人</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="me-4 mb-8">
-                <Card />
+              <div className="me-4 ">
+                {/* card */}
+                <div
+                  className="little-card shadow-sm bg-white rounded main-hover"
+                  onClick={() => {
+                    history.push('/groupDetail/57')
+                  }}
+                >
+                  <img
+                    className="w-100 h-50 rounded-top"
+                    src={require('../../../image/shopList/DianShuiLou-1.jpg')}
+                    alt=""
+                  />
+                  <div className="p-3">
+                    <div className="d-flex justify-content-between">
+                      <h4>點水樓</h4>
+                      <span class="badge rounded-pill bg-primary">台式</span>
+                    </div>
+                    <h6 className="fw-normal">目前人數:0人</h6>
+                    <h6 className="fw-normal mb-4">用餐時間:2022/07/21</h6>
+                    <div class="progress">
+                      <div
+                        class="progress-bar progress-bar-striped"
+                        role="progressbar"
+                        style={{ width: '30%' }}
+                        aria-valuenow="0"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ></div>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                      <p className="progress-text mt-1">剩下5天</p>
+                      <p className="progress-text mt-1">目標人數:3人</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="me-4 mb-8">
-                <Card />
+              <div className="me-4 ">
+                {/* card */}
+                <div
+                  className="little-card shadow-sm bg-white rounded main-hover"
+                  onClick={() => {
+                    history.push('/groupDetail/54')
+                  }}
+                >
+                  <img
+                    className="w-100 h-50 rounded-top"
+                    src={require('../../../image/shopList/Pinnada-1.jpg')}
+                    alt=""
+                  />
+                  <div className="p-3">
+                    <div className="d-flex justify-content-between">
+                      <h4>品田牧場</h4>
+                      <span class="badge rounded-pill bg-primary">日式</span>
+                    </div>
+                    <h6 className="fw-normal">目前人數:0人</h6>
+                    <h6 className="fw-normal mb-4">用餐時間:2022/07/20</h6>
+                    <div class="progress">
+                      <div
+                        class="progress-bar progress-bar-striped"
+                        role="progressbar"
+                        style={{ width: '30%' }}
+                        aria-valuenow="0"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ></div>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                      <p className="progress-text mt-1">剩下7天</p>
+                      <p className="progress-text mt-1">目標人數:5人</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
